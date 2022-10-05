efe52c9a218feaba226cc179d7e3e26a20231818
majd443 <m.mazarib@hotmail.com>
2022-10-05 14:04:43 +0300

    added header and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './components/header/header.component';
-import { HeroComponent } from './componrnts/hero/hero.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroComponent, AboutComponent, ResumeComponent, PortfolioComponent, ContactComponent],
+  declarations: [AppComponent, HeaderComponent, HeroComponent, AboutComponent, ResumeComponent, PortfolioComponent, ContactComponent,],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,18 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   public isActive: boolean = false;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public toggleMenu(): void {
-    this.isActive = !this.isActive
+    this.isActive = !this.isActive;
   }
-
 }
