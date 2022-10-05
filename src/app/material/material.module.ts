7682bc1dfa8e1f5dff7063abc65cb2910e101307
majd443 <m.mazarib@hotmail.com>
2022-10-06 00:05:46 +0300

    added contact and portfolio and resume

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
-const materialModules = [MatIconModule, MatButtonModule];
+
+const materialModules = [MatIconModule, MatButtonModule, MatInputModule];
 
 @NgModule({
   declarations: [],
